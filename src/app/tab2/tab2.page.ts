dd3f220600f93208092c9a5761add4a58cafda9d
dany13gd <dany13gd@gmail.com>
2024-04-01 11:08:40 -0600

    feat(all platforms): save picture in all platforms(iOS,Android and web).

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -37,9 +37,10 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page {
   constructor(public photoService: PhotoService) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
   addPhotoToGallery() {
-    console.log(this.photoService);
-
     this.photoService.addNewToGallery();
   }
 }
